1522b31688891b839e822f251d701386089fb86b
Andrey Nikitin <support@avreg.net>
2021-02-17 10:13:46 +0300

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,32 @@
 module.exports = {
-  extends: ['eslint-config-airbnb-base', 'eslint-config-prettier'],
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            3
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+           "error",
+           { allow: ["warn"] }
+         ]
+    }
 };
