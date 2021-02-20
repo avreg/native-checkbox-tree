468f984c3e48c731294d5da972b46a3887cd5b02
Andrey Nikitin <support@avreg.net>
2021-02-20 12:11:49 +0300

    format

--- webpack.config.js
+++ webpack.config.js
@@ -3,82 +3,82 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin;
+   .BundleAnalyzerPlugin;
 
 module.exports = {
-    mode: 'none',
-    devtool: 'source-map',
-    entry: {
-        tree: './src/index.js',
-        'tree.min': './src/index.js',
-    },
-    output: {
-        filename: '[name].js',
-        libraryExport: 'default',
-        library: 'Tree',
-        libraryTarget: 'umd',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
+   mode: 'none',
+   devtool: 'source-map',
+   entry: {
+      tree: './src/index.js',
+      'tree.min': './src/index.js',
+   },
+   output: {
+      filename: '[name].js',
+      libraryExport: 'default',
+      library: 'Tree',
+      libraryTarget: 'umd',
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+         },
+         {
+            test: /\.less$/,
+            use: [
+               'style-loader',
+               'css-loader',
+               {
+                  loader: 'postcss-loader',
+                  options: {
+                     plugins: [
+                        require('postcss-import'),
+                        require('autoprefixer'),
+                     ],
+                  },
+               },
+               'less-loader',
+            ],
+         },
+      ],
+   },
+   optimization: {
+      minimize: true,
+      minimizer: [
+         new UglifyJSPlugin({
+            include: /\.min\.js$/,
+            cache: true,
+            parallel: true,
+            sourceMap: true,
+            uglifyOptions: {
+               compress: {
+                  warnings: false,
+                  comparisons: false,
+                  drop_console: true,
+               },
+               mangle: {
+                  safari10: true,
+               },
+               output: {
+                  comments: false,
+                  ascii_only: true,
+               },
             },
-            {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                require('postcss-import'),
-                                require('autoprefixer'),
-                            ],
-                        },
-                    },
-                    'less-loader',
-                ],
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new UglifyJSPlugin({
-                include: /\.min\.js$/,
-                cache: true,
-                parallel: true,
-                sourceMap: true,
-                uglifyOptions: {
-                    compress: {
-                        warnings: false,
-                        comparisons: false,
-                        drop_console: true,
-                    },
-                    mangle: {
-                        safari10: true,
-                    },
-                    output: {
-                        comments: false,
-                        ascii_only: true,
-                    },
-                },
-            }),
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
-        new OptimizeCSSAssetsPlugin({
-            assetNameRegExp: /\.min\.css$/,
-        }),
-        new webpack.BannerPlugin(
-            'treejs\n@version 1.8.0\n@see https://github.com/daweilv/treejs'
-        ),
-        new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
-    ],
+         }),
+      ],
+   },
+   plugins: [
+      new MiniCssExtractPlugin({
+         filename: '[name].css',
+      }),
+      new OptimizeCSSAssetsPlugin({
+         assetNameRegExp: /\.min\.css$/,
+      }),
+      new webpack.BannerPlugin(
+         'treejs\n@version 1.8.0\n@see https://github.com/daweilv/treejs'
+      ),
+      new BundleAnalyzerPlugin({ analyzerMode: 'json' }),
+   ],
 };
